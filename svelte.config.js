4ba835406b5fc05a7553ed9cbde1a908c36cf2c1
Mark Manders <mmjmanders@gmail.com>
2026-01-30 16:51:55 +0100

    fix workflows: update setup action path in lint and pages workflows

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 
+const { argv, env } = process;
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			precompress: true
+		}),
+		paths: {
+			base: argv.includes('dev') ? '' : env.BASE_PATH
+		}
+	}
+};
 
 export default config;

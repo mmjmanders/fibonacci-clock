d04289aad214ee0da0d23c1b079a633d8976062c
Mark Manders <mmjmanders@gmail.com>
2026-02-02 10:58:18 +0100

    Feat/pwa (#3)
    * add PWA support: configure Vite PWA plugin and update layout with manifest
    
    * add icons for PWA support
    
    * add apple touch icon for enhanced PWA support
    
    * add manifest icons for PWA support
    
    * update dependencies in package.json and pnpm-lock.yaml
    
    * add XML Prettier plugin and update SVG formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import appleTouchIcon from '$lib/assets/apple-touch-icon-180x180.png';
+	import { pwaInfo } from 'virtual:pwa-info';
 
 	let { children } = $props();
+	const manifest = $derived(pwaInfo?.webManifest?.href);
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} type="image/svg+xml" />
+	<link rel="apple-touch-icon" href={appleTouchIcon} />
+	{#if manifest}
+		<link rel="manifest" href={manifest} />
+	{/if}
+</svelte:head>
+
 {@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,45 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		SvelteKitPWA({
+			devOptions: {
+				enabled: true
+			},
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'Fibonacci Clock',
+				short_name: 'Fibonacci Clock',
+				display: 'standalone',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				]
+			}
+		})
+	]
+});
